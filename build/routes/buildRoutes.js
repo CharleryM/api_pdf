99521ccc3f811e22c18da76846e2ff173f194e87
CharleryM <charlerymalcolm.l@google.com>
2024-02-21 22:17:02 +0100

    update

--- build/routes/buildRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.buildRoutes = void 0;
-const facturesRouter_1 = require("./facturesRouter");
-function buildRoutes(app) {
-    app.use("/facture", facturesRouter_1.factureRouter);
-}
-exports.buildRoutes = buildRoutes;
--- src/routes/buildRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Express } from "express";
-import { factureRouter } from "./facturesRouter";
-
-export function buildRoutes(app: Express) {
-    app.use("/facture", factureRouter);
-}
-
