22a6c4caa5fa651f04005e9442b9b6e43edbea65
CharleryM <charlerymalcolm.l@google.com>
2024-02-21 22:12:51 +0100

    update

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { connection } from './config/database'
 import { Facture } from './models/factureModel';
 import { buildRoutes } from './routes/buildRoutes';
@@ -11,6 +11,10 @@ const port = 3000
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:4200" }));
+
+app.set('view engine', 'ejs'); 
+app.set('views', './views');
+
 
 buildRoutes(app);
 
@@ -28,7 +32,7 @@ const server = app.listen(port, function () {
   console.log(`Listening on ${port}`);
 });
 
-app.use('/', (__res, req) => {
-  return req.send('hello world')
-})
+app.get('/', (req: Request, res: Response) => {
+  res.render('index'); 
+});
 export { app, server }
